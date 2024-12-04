fb50d0e85288d149d167e86abc5bbf0bde42bb37
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-12-04 18:00:43 +0600

    server js  :airplane:

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 require('dotenv').config()
-
+// server js
 const mail = 'hazrataliein@gmail.com';
 const password = 'abvk dimq kzxk fulk';
 
